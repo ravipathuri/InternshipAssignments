9cb4cdad70db618455187e6ea3ee576913b4eca5
ravipathuri <ravichinna1231@gmail.com>
2023-08-07 10:13:02 +0530

    Intial Demo commit

---
+++ Assignment1_1/IsEqual.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cctype>
+#include "IsEqual.h"
+
+bool areEqual(const char* str1, const char* str2, bool caseInsensitive) {
+    int i = 0;
+    while (str1[i] != '\0' && str2[i] != '\0') {
+        if (caseInsensitive) { // Convert characters to lowercase before comparison
+            char c1 = tolower(str1[i]);
+            char c2 = tolower(str2[i]);
+            if (c1 != c2)
+                return false;
+        }
+        else {
+            if (str1[i] != str2[i])
+                return false;
+        }
+        i++;
+    }
+    if (str1[i] == '\0' && str2[i] == '\0')// Check if both strings have reached the end
+        return true;
+    return false;
+}
+
+int displayResult(bool y) {
+    if (y) {
+        std::cout << "Strings are equal." << std::endl;
+        return 1;
+    }
+    else {
+        std::cout << "Strings are not equal." << std::endl;
+        return 2;
+    }
+    return 0;
+}
---
+++ Assignment1_1/IsEqual.h
@@ -0,0 +1,3 @@
+#pragma once
+bool areEqual(const char*, const char*, bool);
+int displayResult(bool);
